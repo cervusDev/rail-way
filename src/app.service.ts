e7ebaf1d47ce554b9d2c1110618c47d2c39d886f
cervusDev <gustavo.cervus@gmail.com>
2024-12-05 16:28:12 -0300

    injeção de classe swagger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Injectable()
 export class AppService {
+  @ApiOperation({ summary: 'Obtém todos os usuários' })
   getHello(): string {
     return 'Olá mundo!';
   }
